a8fd6169c6aecbf4437ac1550b9add5db9536eef
Jannis Köckritz <jannis.koeckritz@gmx.de>
2018-09-21 17:21:47 +0200

    Add port to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,6 +11,7 @@ Rails.application.configure do
   config.eager_load = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'projex-bb17.herokuapp.com', :protocol => 'https' }
+  config.action_mailer.smtp_settings = { port: 25 }
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
