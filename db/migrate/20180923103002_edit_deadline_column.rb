69c32b9adb9d8f3eb78a1d336be4cb42a731201b
Jannis Köckritz <jannis.koeckritz@gmx.de>
2018-09-23 13:00:55 +0200

    Delete old migration

--- db/migrate/20180923103002_edit_deadline_column.rb
+++ db/migrate/20180923103002_edit_deadline_column.rb
@@ -1,5 +1,5 @@
 class EditDeadlineColumn < ActiveRecord::Migration[5.1]
   def change
-  	change_column :projects, :deadline, :datetime, :default => DateTime.now + 1.week, :null => false
+  	change_column :projects, :deadline, :datetime, :default => DateTime.now, :null => false
   end
 end
--- db/migrate/20180923105619_edit_deadline_again.rb
+++
@@ -1,5 +0,0 @@
-class EditDeadlineAgain < ActiveRecord::Migration[5.1]
-  def change
-  	change_column :projects, :deadline, :datetime, :null => false
-  end
-end
